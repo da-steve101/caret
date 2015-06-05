60fc8df54a45799a561a81ab68a0ccd93c662891
Stephen Tridgell <stephen@tridgell.net>
2015-06-06 08:52:12 +1000

    Added norma to caret, changed olk to use the lev function so returns a factor in prediction

--- models/files/norma.R
+++ models/files/norma.R
@@ -1,51 +1,51 @@
-modelInfo <- list(label = "OLK with Radial Basis Function Kernel",
+modelInfo <- list(label = "NORMA with Radial Basis Function Kernel",
                   library = "kernlab",
                   type = c("Classification", "Regression"),
-                  parameters = data.frame(parameter = c('sigma', 'C', 'r', 'epsilon', 'buffersize'),
+                  parameters = data.frame(parameter = c('sigma', 'nu', 'lambda', 'eta', 'buffersize'), 
                                           class = c("numeric", "numeric", "numeric", "numeric", "integer"),
-                                          label = c("Sigma", "Cost", "Forgetting factor", "Regression Error Tol", "Buffersize")),
+                                          label = c("Sigma", "Nu", "Regularization", "Learning Rate", "Buffersize")),
                   grid = function(x, y, len = NULL) {
                         library(kernlab)
                         sigmas <- sigest(as.matrix(x), na.action = na.omit, scaled = TRUE)
                         if (length(unique(y)) <= 2)
                             expand.grid(sigma = mean(as.vector(sigmas[-2]))*(c(1,5,10,50,100)),
-                                        C = 2 ^((1:len) - 3), r = 10^(-5:-1), epsilon=0, buffersize=1000)
+                                        nu = (1:8)/10, lambda = (1 - 2^(-4:-1)), eta = 10^(-3:-1), buffersize=1000)
                         else
                             expand.grid(sigma = mean(as.vector(sigmas[-2]))*(c(1,5,10,50,100)),
-                                        C = 2 ^((1:len) - 3), r = 10^(-5:-1), epsilon= 10^(-3:-1), buffersize=1000)
+                                        nu = (1:8)/10, lambda = (1 - 2^(-4:-1)), eta = 10^(-3:-1), buffersize=1000)
                   },
                   loop = NULL,
                   fit = function(x, y, wts, param, lev, last, classProbs, ...) {
                     if (length(unique(y)) <= 2)
-                        out <- initOLK(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
+                        out <- inlearn(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
                                        type = "classification", buffersize = param$buffersize)
                     else
-                        out <- initOLK(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
+                        out <- inlearn(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
                                        type = "regression", buffersize = param$buffersize)
-                    out <- olk(out, x = as.matrix(x), y = y, C = param$C, r = param$r, epsilon = param$epsilon)
+                    out <- onlearn(out, x = as.matrix(x), y = y, nu = param$nu, lambda = param$lambda, eta = param$eta)
                     out
                     },
                   predict = function(modelFit, newdata, submodels = NULL) {
-                    olkPred <- function(obj, x)
+                    NORMAPred <- function(obj, x)
                     {
                       pred <- predict(obj, x)
                       if (length(lev(obj)) == 2) {
-                          # two class classification
+                          # two class classification      
                           pred <- factor((pred > 0)*1 + 1)
                           levels(pred) <- lev(obj)
                       }
                       pred
                     }
-                    out <- try(olkPred(modelFit, newdata), silent = FALSE)
+                    out <- try(NORMAPred(modelFit, newdata), silent = FALSE)
                     out
                   },
                   prob = function(modelFit, newdata, submodels = NULL) {
-                    olkPred <- function(obj, x)
+                    NORMAPred <- function(obj, x)
                     {
                       pred <- predict(obj, x)
                       pred
                     }
-                    out <- try(olkPred(modelFit, newdata), silent = FALSE)
+                    out <- try(NORMAPred(modelFit, newdata), silent = FALSE)
                     if (length(lev(modelFit)) <= 2) {
                         # two class classification
                         # scale between 0 and 1 with sigmoid, scaling power? use sigma?
@@ -69,8 +69,5 @@ modelInfo <- list(label = "OLK with Radial Basis Function Kernel",
                   tags = c("Kernel Method", "Online Learning", "Radial Basis Function"),
                   levels = function(x) lev(x),
                   sort = function(x) {
-                    # If the cost is high, the decision boundary will work hard to
-                    # adapt. Also, if C is fixed, smaller values of sigma yeild more
-                    # complex boundaries
-                    x[order(x$C),]
+                    x[order(x$nu),]
                   })
