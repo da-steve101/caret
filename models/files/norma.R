ff405ac748fb75e62d83596809640cc1f84f5d29
stephen <stephen@serenity.staff.usyd.edu.au>
2015-07-02 10:39:08 +0800

    Bug fixes to norma file, added regression to suite

--- models/files/norma.R
+++ models/files/norma.R
@@ -1,30 +1,26 @@
 modelInfo <- list(label = "NORMA with Radial Basis Function Kernel",
                   library = "kernlab",
                   type = c("Classification", "Regression"),
-                  parameters = data.frame(parameter = c('sigma', 'nu', 'lambda', 'eta', 'buffersize'), 
-                                          class = c("numeric", "numeric", "numeric", "numeric", "integer"),
-                                          label = c("Sigma", "Nu", "Regularization", "Learning Rate", "Buffersize")),
+                  parameters = data.frame(parameter = c('sigma', 'nu', 'lambda', 'eta', 'buffersize', 'normaType', 'kernel'), 
+                                          class = c("numeric", "numeric", "numeric", "numeric", "integer", "character", "character"),
+                                          label = c("Sigma", "Nu", "Regularization", "Learning Rate", "Buffersize", "Type", "Kernel Function")),
                   grid = function(x, y, len = NULL) {
                         library(kernlab)
                         sigmas <- sigest(as.matrix(x), na.action = na.omit, scaled = TRUE)
-                        if (length(unique(y)) <= 2)
-                            expand.grid(sigma = mean(as.vector(sigmas[-2]))*(c(1,5,10,50,100)),
-                                        nu = (1:8)/10, lambda = (1 - 2^(-4:-1)), eta = 10^(-3:-1), buffersize=1000)
+                        normaGrid<-expand.grid(sigma = mean(as.vector(sigmas[-2]))*(c(1,5,10,50,100)),                                               nu = (1:8)/10, lambda = (1 - 2^(-4:-1)),
+                                               eta = 10^(-3:-1), buffersize=1000)
+                        normaGrid$kernel <- "rbfkernel"
+                        if (length(unique(y)) <= 1)
+                            normaGrid$normaType <- "novelty"
+                        else if (length(unique(y)) <= 2)
+                            normaGrid$normaType <- "classification"
                         else
-                            expand.grid(sigma = mean(as.vector(sigmas[-2]))*(c(1,5,10,50,100)),
-                                        nu = (1:8)/10, lambda = (1 - 2^(-4:-1)), eta = 10^(-3:-1), buffersize=1000)
+                            normaGrid$normaType <- "regression"
                   },
                   loop = NULL,
                   fit = function(x, y, wts, param, lev, last, classProbs, ...) {
-                    if (length(unique(y)) <= 1)
-                        out <- inlearn(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
-                                       type = "novelty", buffersize = param$buffersize)
-                    else if (length(unique(y)) <= 2)
-                        out <- inlearn(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
-                                       type = "classification", buffersize = param$buffersize)
-                    else
-                        out <- inlearn(d = dim(x)[2], kernel = "rbfdot", kpar = list(param$sigma),
-                                       type = "regression", buffersize = param$buffersize)
+                    out <- inlearn(d = dim(x)[2], kernel = param$kernel, kpar = list(param$sigma),
+                                   type = param$normaType, buffersize = param$buffersize)
                     out <- onlearn(out, x = as.matrix(x), y = y, nu = param$nu, lambda = param$lambda, eta = param$eta)
                     out
                     },
